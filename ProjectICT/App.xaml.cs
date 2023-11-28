c6f2e5ad021be9de7d746d7114f3348f3cd14204
michi <michielooghe@gmail.com>
2023-11-28 16:00:34 +0100

    Add project files.

---
+++ ProjectICT/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProjectICT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
