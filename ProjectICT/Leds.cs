485e7c407b59709ded09aeb225bd29164008a461
Michiel Ooghe <michielooghe@gmail.com>
2023-12-05 00:02:27 +0100

    Poging1 met microcontrollen en c sharp elk een eigen aantsuring van de leds.

---
+++ ProjectICT/Leds.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Threading;
+
+namespace ProjectICT
+{
+    class Leds
+    {
+        Brush filledBrush = Brushes.Yellow;
+        Brush transparentBrush = Brushes.Transparent;
+
+
+        private bool toggle1;
+
+		public bool  Toggle1
+		{
+			get { return toggle1; }
+			set { toggle1 = value; }
+		}
+
+		private bool toggle2;
+
+        public bool Toggle2
+        {
+            get { return toggle2; }
+            set { toggle2 = value; }
+        }
+
+
+        private bool toggle3;
+
+        public bool Toggle3
+        {
+            get { return toggle3; }
+            set { toggle3 = value; }
+        }
+
+        private bool toggle4;
+
+        public bool Toggle4
+        {
+            get { return toggle4; }
+            set { toggle4 = value; }
+        }
+
+
+        private int teller;
+
+
+		private int comData;
+
+		public int ComData
+		{
+			get 
+			{ 
+				return comData;
+			}
+			set 
+			{ 
+				comData = value;
+				Toggle();
+			}
+		}
+
+		private bool led1;
+
+		public bool Led1
+		{
+			get { return led1; }
+			set { led1 = value; }
+		}
+
+        private bool led2;
+
+        public bool Led2
+        {
+            get { return led2; }
+            set { led2 = value; }
+        }
+
+        private bool led3;
+
+        public bool Led3
+        {
+            get { return led3; }
+            set { led3 = value; }
+        }
+
+        private bool led4;
+
+        public bool Led4
+        {
+            get { return led4; }
+            set { led4 = value; }
+        }
+
+        private bool led5;
+
+        public bool Led5
+        {
+            get { return led5; }
+            set { led5 = value; }
+        }
+
+        private bool led6;
+
+        public bool Led6
+        {
+            get { return led6; }
+            set { led6 = value; }
+        }
+
+        private bool led7;
+
+        public bool Led7
+        {
+            get { return led7; }
+            set { led7 = value; }
+        }
+
+        private bool led8;
+
+        public bool Led8
+        {
+            get { return led8; }
+            set { led8 = value; }
+        }
+
+
+
+        public void Reset()
+		{
+			ByteToLeds(0);
+			teller = 0;
+		
+		}
+
+		private void Toggle()
+		{
+			if (comData == 1) 
+			{ 
+				toggle1 = !toggle1;
+				toggle2 = false;
+				toggle3 = false;
+				toggle4 = false;
+
+			}
+			if (comData == 2) 
+			{
+				toggle2 = !toggle2;
+                toggle1 = false;
+                toggle3 = false;
+                toggle4 = false;
+            }
+			if (comData == 3) 
+			{
+				toggle3 = !toggle3;
+                toggle1 = false;
+                toggle2 = false;
+                toggle4 = false;
+            }
+			if (comData == 4)
+			{ 
+				toggle4 = !toggle4;
+                toggle1 = false;
+                toggle2 = false;
+                toggle3 = false;
+            }
+		
+		}
+
+
+		public void Aansturen()
+		{
+			if (toggle1 == true)
+			{
+				if (teller == 0 || teller ==9) { teller = 1; }
+				
+				ByteToLeds(teller);
+				teller++;
+				
+			}
+			if (toggle2 == true)
+			{
+				
+
+			}
+			if (toggle3 == true)
+			{
+
+			}
+			if (toggle4 == true)
+			{
+
+			}
+
+
+		}
+
+		private void ByteToLeds (int data)
+		{
+			
+
+
+            if (data == 1 )
+			{
+				led1 = true;
+                
+            }
+			else
+			{
+                led1 = false;
+            }
+                
+
+            if (data == 2)
+            {
+				led2 = true;
+			}  
+            else
+			{
+				led2 = false;
+			}
+                
+
+            if (data == 3)
+            {
+				led3 = true;
+			}    
+            else
+			{
+				led3 = false;
+			}
+                
+
+            if (data == 4)
+            {
+				led4 = true;
+			}   
+            else
+			{
+				led4 = false;
+			}
+                
+
+            if (data == 5)
+            {
+				led5 = true;
+			}   
+            else
+			{
+				led5 = false;
+			}
+                
+
+            if (data == 6)
+            {
+				led6 = true;
+			}   
+            else
+			{
+				led6 = false;
+			}
+                
+
+            if (data == 7)
+            {
+				led7 = true;
+			} 
+            else
+			{
+				led7 = false;
+			}
+                
+
+            if (data == 8)
+            {
+				led8 = true;
+			}  
+            else
+			{
+				led8 = false;
+			}
+			
+			
+                
+        }
+
+    }
+
+
+	
+}
--- ProjectICT/MainWindow.xaml.cs
+++ ProjectICT/MainWindow.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -16,9 +22,118 @@ namespace ProjectICT
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort _serialPort;
+        Leds _leds;
+
+        private DispatcherTimer _timer;
+
+        Brush filledBrush = Brushes.Yellow;
+        Brush transparentBrush = Brushes.Transparent;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(100);
+            _timer.Tick += Timer_Tick;
+
+
+            _serialPort = new SerialPort();
+            _leds = new Leds();
+
+            // Komt er data binnen? Ga verder naar de event handler.
+            _serialPort.DataReceived += _serialPort_DataReceived;
+
+            cbxComPorts.Items.Add("None");
+            foreach (string s in SerialPort.GetPortNames())
+                cbxComPorts.Items.Add(s);
+        }
+
+        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Lees alle tekst binnen tot er een new line passeert... (\n).
+            string receivedText = _serialPort.ReadLine().Trim();
+
+            //// Poging met error...
+            //// Plaats de ontvangen dat op het Label.
+            //lblReceivedData.Content = receivedText;
+
+            // Geef de data door aan een thread van de GUI om zo 
+            // het Label aan te passen.
+            Dispatcher.Invoke(new Action<string>(Update), receivedText);
+        }
+
+        private void Update(string text)
+        {
+            _leds.ComData = Convert.ToInt32(text);
+
+            if(_leds.Toggle1 == true) { _timer.Start(); }
+            
+            
+
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            _leds.Aansturen();
+            LedUpdate();
+            if (_leds.Toggle1 == false && _leds.Led8 == true) { _timer.Stop(); _leds.Reset(); }
+        }
+
+        private void LedUpdate()
+        {
+            if (_leds.Led1 == true) { Cirkel1.Fill = filledBrush; }
+            else { Cirkel1.Fill = transparentBrush; }
+
+            if (_leds.Led2 == true) { Cirkel2.Fill = filledBrush; }
+            else { Cirkel2.Fill = transparentBrush; }
+
+            if (_leds.Led3 == true) { Cirkel3.Fill = filledBrush; }
+            else { Cirkel3.Fill = transparentBrush; }
+
+            if (_leds.Led4 == true) { Cirkel4.Fill = filledBrush; }
+            else { Cirkel4.Fill = transparentBrush; }
+
+            if (_leds.Led5 == true) { Cirkel5.Fill = filledBrush; }
+            else { Cirkel5.Fill = transparentBrush; }
+
+            if (_leds.Led6 == true) { Cirkel6.Fill = filledBrush; }
+            else { Cirkel6.Fill = transparentBrush; }
+
+            if (_leds.Led7 == true) { Cirkel7.Fill = filledBrush; }
+            else { Cirkel7.Fill = transparentBrush; }
+
+            if (_leds.Led8 == true) { Cirkel8.Fill = filledBrush; }
+            else { Cirkel8.Fill = transparentBrush; }
+
+            
+
+
+        }
+
+        
+
+        private void cbxComPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_serialPort != null)
+            {
+                if (_serialPort.IsOpen)
+                    _serialPort.Close();
+
+                if (cbxComPorts.SelectedItem.ToString() != "None")
+                {
+                    _serialPort.PortName = cbxComPorts.SelectedItem.ToString();
+                    _serialPort.Open();
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_serialPort != null)
+                _serialPort.Dispose();
         }
     }
 }
